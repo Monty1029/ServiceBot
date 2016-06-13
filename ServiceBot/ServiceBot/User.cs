2ce1dcc49ae167d0711f5855d3b553904f9d682d
Monty Dhanani <dhanani.monty@gmail.com>
2016-06-13 19:54:56 -0400

    Added User, Avatar, and Password classes
    Started working on User class

---
+++ ServiceBot/ServiceBot/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceBot
+{
+    public class User
+    {
+        private string name;
+        private int id;
+        private Avatar avatar;
+        private Password password;
+
+        public User(string name, int id, Avatar avatar, Password password)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public Avatar getAvatar()
+        {
+            return avatar;
+        }
+
+        public void setAvatar()
+        {
+
+        }
+
+        public Password getPassword()
+        {
+            return password;
+        }
+
+        public void setPassword()
+        {
+
+        }
+    }
+}

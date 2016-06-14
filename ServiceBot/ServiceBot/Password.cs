a94ce7379ca74e0dad0d6992bdc3b0bed4f363e1
Monty Dhanani <dhanani.monty@gmail.com>
2016-06-14 19:17:15 -0400

    Hashing password

--- ServiceBot/ServiceBot/Password.cs
+++ ServiceBot/ServiceBot/Password.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 
 namespace ServiceBot
 {
     public class Password
     {
+        string password;
+
         public Password()
         {
+            
+        }
 
+        public void hashPassword()
+        {
+            var data = new byte[32];
+            var sha1 = new SHA1CryptoServiceProvider();
+            var sha1data = sha1.ComputeHash(data);
         }
     }
 }
